5e328b0edd526a276c1816301de8d83dc0d78105
Alesa95 <alejandra_santiago_95@hotmail.com>
2022-09-26 17:10:53 +0200

    Limpieza de código

--- 04_base_de_datos_mysqli/index_basico.php
+++ 04_base_de_datos_mysqli/index_basico.php
@@ -12,17 +12,6 @@
     <h1>Listado de películas</h1>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
-    <!--<form action="" method="post">
-        <div class="form-group">
-            <label for="exampleInputEmail1">Título</label>
-            <input type="text" name="titulo" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Introduzca el título">
-        </div>
-        <div class="form-group">
-            <label for="exampleInputPassword1">Fecha de estreno</label>
-            <input type="date" name="fecha_estreno" class="form-control" id="exampleInputPassword1">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>-->
     <?php 
         require 'insertar_pelicula.php'; 
     ?>
@@ -44,19 +33,3 @@
     ?>
 </body>
 </html>
-
-<?php
-    /*if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        //  se puede poner if ($_POST) y fin
-        $titulo = $_POST["titulo"];
-        $fecha_estreno = $_POST["fecha_estreno"];
-        $sql = "INSERT INTO peliculas (titulo, fecha_estreno)
-                    VALUES ('$titulo', '$fecha_estreno')";
-
-        if ($conexion -> query($sql) === TRUE) {
-            echo "New record created successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . $conexion -> error;
-        }
-    }*/
-?>  
