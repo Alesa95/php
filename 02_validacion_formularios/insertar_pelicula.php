a4a43c6de2900e20ffc0923f3b06f28d9dfcd3d4
Alesa95 <alejandra_santiago_95@hotmail.com>
2022-10-13 21:06:48 +0200

    Primer commit

---
+++ 02_validacion_formularios/insertar_pelicula.php
@@ -0,0 +1,71 @@
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && 
+            $_POST["formulario"] == "insertar") {
+
+        //  Validamos el título
+        if (empty($_POST["titulo"])) {
+            $err_titulo = "*El título es obligatorio";
+        } else {
+            $temp_titulo = $_POST["titulo"];
+
+            if (strlen($temp_titulo) > 40) {
+                $err_titulo = "*El título debe tener menos de 40 caracteres";
+            } else {
+                $titulo = $temp_titulo;
+            }
+        }
+        //  Validamos la duración
+        if (empty($_POST["duracion"])) {
+            $err_duracion = "*La duración es obligatoria";
+        } else {
+            $temp_duracion = $_POST["duracion"];
+            $temp_duracion = filter_var($temp_duracion, FILTER_VALIDATE_INT);
+            
+            if (!$temp_duracion) {
+                $err_duracion = "*La duración debe ser un número";
+            } else {
+                if ($temp_duracion <= 0) {
+                    $err_duracion = "*La duración debe ser mayor que cero";
+                } else {
+                    $duracion = $_POST["duracion"];
+                }
+            }
+        }
+        //  Validamos la fecha de estreno
+        if (empty($_POST["fecha_estreno"])) {
+            $err_fecha_estreno = "*La fecha de estreno es obligatoria";
+        } else {
+            $fecha_estreno = $_POST["fecha_estreno"];
+        }
+
+        if (isset($titulo) && isset($duracion) && isset($fecha_estreno)) {
+
+            $sql = "INSERT INTO peliculas (titulo, duracion, fecha_estreno)
+            VALUES ('$titulo', '$duracion', '$fecha_estreno')";
+
+            if ($conexion -> query($sql) == TRUE) {
+                echo "<p>Fila insertada</p>";
+            } else {
+                echo "<p>Error al insertar</p>";
+            }
+        }
+    }
+?>
+
+<form action ="" method="post">
+    <label>Título</label>
+    <input type="text" name="titulo">
+    <?php if (isset($err_titulo)) echo $err_titulo; ?>
+    <br><br>
+    <label>Duración</label>
+    <input type="text" name="duracion">
+    <?php if (isset($err_duracion)) echo $err_duracion; ?>
+    <br><br>
+    <label>Fecha de estreno</label>
+    <input type="date" name="fecha_estreno">
+    <?php if (isset($err_fecha_estreno)) echo $err_fecha_estreno; ?>
+    <br><br>
+    <input type="hidden" name="formulario" value="insertar">
+    <input type="submit" value="Insertar película">
+</form>
+
---
+++ 02_validacion_formularios/videojuegos/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Videojuegos</title>
+</head>
+<body>
+    <h1>Videojuegos</h1>
+
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $temp_nombre = $_POST["nombre"];
+            $temp_precio = $_POST["precio"];
+            if (isset($_POST["consola"])) $temp_consola = $_POST["consola"];
+            
+
+            if (empty($temp_nombre)) {
+                $err_nombre = "*El nombre es obligatorio";
+            } 
+
+            if (empty($temp_precio)) {
+                $err_precio = "*El precio es obligatorio";
+            } 
+
+            if (empty($temp_consola)) {
+                $err_consola = "*Selecciona una consola";
+            }
+        }
+    ?>
+
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+        Nombre: <input type="text" name="nombre">
+        <span class="error"><?php if (isset($err_nombre)) echo $err_nombre; ?></span>
+        <br><br>
+        Precio: <input type="text" name="precio">
+        <span class="error"><?php if (isset($err_precio)) echo $err_precio; ?></span>
+        <br><br>
+        <select name="consola">
+            <option value="" selected disabled hidden>Selecciona consola</option>
+            <option value="ps4">Playstation 4</option>
+            <option value="ps5">Playstation 5</option>
+            <option value="switch">Nintendo Switch</option>
+        </select>
+        <span class="error"><?php if (isset($err_consola)) echo $err_consola; ?></span>
+        <br><br>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
