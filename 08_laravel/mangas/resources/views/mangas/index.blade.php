bdce8e5d35602f7a0fb67b4e338ed7c76a660d38
Alesa95 <alejandra_santiago_95@hotmail.com>
2022-11-23 12:44:26 +0100

    Cambios menores y modo oscuro

--- 08_laravel/mangas/resources/views/mangas/index.blade.php
+++ 08_laravel/mangas/resources/views/mangas/index.blade.php
@@ -5,9 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mangas</title>
+    <!-- Bootstrap 5.2.2 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!-- JQuery 3.6.1 -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script> 
 </head>
 <body>
+    <script>
+        function darkMode () {
+            $("table").removeClass("table")
+            $("table").addClass("table table-dark");
+        }
+    </script>
+
     <div class="container">
         <h1>{{ $mensaje }}</h1>
 
@@ -36,7 +46,15 @@
 
         <br><br>
 
-        <table class="table table-primary">
+        <h2>Listado de mangas</h2>
+
+        <br>
+
+        <button class="btn btn-secondary" onclick="darkMode()">Modo Oscuro</button> 
+
+        <br><br>
+
+        <table class="table">
             <thead>
                 <tr>
                     <th>Título</th>
@@ -57,7 +75,7 @@
                         </td>
                         <td>
                             <form method="post" action="{{ route('mangas.destroy', ['manga' => $manga -> id]) }}">
-                                {{ csrf_field() }}
+                                @csrf
                                 {{ method_field('DELETE') }}
                                 <button class="btn btn-danger" type="submit">Borrar</button>
                             </form>
