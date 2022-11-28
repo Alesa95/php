fcb75f4bde958cee422db64fd551c18ec09240f9
Alesa95 <alejandra_santiago_95@hotmail.com>
2022-11-28 19:22:57 +0100

    Modo oscuro con cookies arreglado

--- 08_laravel/mangas/resources/views/mangas/show.blade.php
+++ 08_laravel/mangas/resources/views/mangas/show.blade.php
@@ -5,10 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $manga -> titulo }}</title>
+    <!-- Bootstrap 5.2.2 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!-- JQuery 3.6.1 -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script> 
 </head>
-<body>
+<body onload="checkMode();">
     <div class="container">
+        @include('header')
+
         <div class="row">
             <h1>Información de {{ $manga -> titulo }}</h1>
             <div class="col-9">
