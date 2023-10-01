38de3c08ae6cb719dc0c7db807fd8d46321c6566
Alesa95 <alejandra_santiago_95@hotmail.com>
2023-10-01 22:38:45 +0200

    Cosas añadidas

--- 01_estructuras_de_control/funciones.php
+++ 01_estructuras_de_control/funciones.php
@@ -16,12 +16,24 @@
         foreach($palabras as $palabra) {
             $resultado .= $palabra;
         }
+        return $resultado;
+    }
+
+    function sumaNumeros (int|float $num1, int|float $num2) {
+        return $num1 + $num2;
+    }
+
+    function sumaNumerosResEntero (int|float $num1, int|float $num2) : int {
+        return $num1 + $num2;
     }
     ?>
 
     <?php
     echo "<h3>" . sumaDos(3) . "</h3>";
     echo "<h3>" . sumaDos(1,3) . "</h3>";
+    echo "<h3>" . concatena("Hola", " ", "mundo!") . "</h3>";
+    echo "<h3>" . sumaNumeros(1.5,3) . "</h3>";
+    echo "<h3>" . sumaNumerosResEntero(1.5,3) . "</h3>";
     ?>
 </body>
 </html>
