9cc90835d57c9a5cc407ad85dc29fbfe4e79731d
Alejandra <alejandra_santiago_95@hotmail.com>
2024-01-30 00:00:58 +0100

    cosis

--- 09_apis/search_dog.php
+++ 09_apis/search_dog.php
@@ -57,12 +57,10 @@
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $response = curl_exec($curl);
         curl_close($curl);
-        //var_dump($response);
     
         $array = json_decode($response, TRUE); 
     
         $imagen = $array['message'];
-    
     }
     ?>
     <img src="<?php echo $imagen ?>"> 
